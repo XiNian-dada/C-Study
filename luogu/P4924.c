bf4588279b89129d4454930aeaaaf28727504bf5
XiNian_dada <2188349576@qq.com>
2025-11-23 23:50:00 +0800

    feat(luogu): 实现矩阵旋转操作功能
    - 添加了读取矩阵大小和操作数量的逻辑
    - 初始化了按顺序填充的n×n矩阵
    - 实现了读取并存储多个旋转操作的功能
    - 完成了基于中心点和长度的子矩阵提取
    - 实现了顺时针和逆时针两种旋转方式
    - 添加了旋转后的矩阵输出功能
    - 包含了示例注释和坐标变换说明

---
+++ luogu/P4924.c
@@ -0,0 +1,73 @@
+#include "stdio.h"
+/*https://www.luogu.com.cn/problem/P4924*/
+int main(){
+    int m,n;
+    scanf ("%d %d",&n,&m);
+    int matrix[n][n];
+    int x = 1;
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            matrix[i][j] = x++;
+        }
+    }
+
+    int operation[m][4];
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < 4; j++){
+            scanf("%d",&operation[i][j]);
+        }
+    }
+    for(int i = 0; i < m; i++){
+        int cx = operation[i][0] - 1;  // 旋转的横坐标
+        int cy = operation[i][1] - 1;  // 旋转的中心点
+        int len = operation[i][2]; // 旋转的长度
+        int ro = operation[i][3]; // 0: 顺时针 1: 逆时针
+        int L = 2*len+1;
+        int temp[L][L];
+        for(int p = 0; p < L; p++){
+            for(int q = 0; q < L; q++){
+                temp[p][q] = matrix[cx - len + p][cy - len + q];
+            }
+        }
+        if (ro == 0){
+            for (int p = 0; p < L; p++){
+                for(int q = 0; q < L; q++){
+                    matrix[cx - len + p][cy - len + q] = temp[L - 1 - q][p];
+                }
+            }
+        }else{
+            for (int p = 0; p < L; p++){
+                for(int q = 0; q < L; q++){
+                    matrix[cx - len + p][cy - len + q] = temp[q][L - 1 - p];
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+
+/*
+sample temp
+
+x1y1 x2y1 x3y1  x4y1 x5y1
+x1y2 x2y2 x3y2  x4y2 x5y2
+x1y3 x2y3 x3y3* x4y3 x5y3
+x1y4 x2y4 x3y4  x4y4 x5y4
+x1y5 x2y5 x3y5  x4y5 x5y5
+
+rotation r;
+
+先转最外层 坐标变换 x y -> y r-1-x
+
+先镜像 再交换
+
+或者先交换 在镜像
+*/
